43ec26d4d3fd0cc1dd43abed6742b7652ca05820
TranQuocToan1996 <tranquoctoan.ce@gmail.com>
2025-11-29 19:28:34 +0700

    Feat: Add index

--- pkg/db/migrations/000001_init.up.sql
+++ pkg/db/migrations/000001_init.up.sql
@@ -2,8 +2,10 @@ CREATE TABLE IF NOT EXISTS shorten_urls (
     id BIGSERIAL PRIMARY KEY,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    code VARCHAR(255) UNIQUE NULL,
+    code VARCHAR(255) Unique NULL,
     status VARCHAR(255) NOT NULL DEFAULT 'submitted', -- submitted, encoded, failed
     algo VARCHAR(255) NULL,
-    clean_url varchar(2048) NOT NULL
+    clean_url varchar(2048) Unique NOT NULL
 );
+
+CREATE INDEX IF NOT EXISTS idx_shorten_urls_status_created_at ON shorten_urls(status, created_at DESC);
